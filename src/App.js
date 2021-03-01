88a2aecce3f18facfd494d015d948b7576b43688
Saidul Amin <saidulnobin@gmail.com>
2021-03-01 18:30:27 +0600

    added optimizely

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  OptimizelyProvider,
+  OptimizelyExperiment,
+  OptimizelyVariation,
+  createInstance,
+} from "@optimizely/react-sdk";
+import AppWrapper from "./components/Wrapper/AppWrapper";
+
+const opInstance = createInstance({
+  sdkKey: "QYHzLSWgc9K6rp7LnLAnj",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OptimizelyProvider
+      optimizely={opInstance}
+      user={{ id: "QYHzLSWgc9K6rp7LnLAnj" }}
+    >
+      <OptimizelyExperiment experiment="button_test">
+        <OptimizelyVariation variation="sum">
+          <h2>Sum Values Feature</h2>
+        </OptimizelyVariation>
+        <OptimizelyVariation variation="multiply">
+          <h2>Multiply Values Feature</h2>
+        </OptimizelyVariation>
+        <OptimizelyVariation default>
+          <h2>Static old Values Feature</h2>
+        </OptimizelyVariation>
+      </OptimizelyExperiment>
+    </OptimizelyProvider>
   );
 }
 
